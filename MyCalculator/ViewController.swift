faf6f1e51b9f0da356a63f14999ec494a2bde042
Naoko Hishinuma <NAOKO@naoko-no-macbook.local>
2017-01-09 03:38:37 +0900

    Update
    演算のエラーを修正。　

--- MyCalculator/ViewController.swift
+++ MyCalculator/ViewController.swift
@@ -11,16 +11,15 @@ import UIKit
 //入力される数
 var input: String = ""
 //途中結果
-var calc_num: Int = 0
-//オリジナル(★名前要検討)
-var org: String = ""
-//演算のマーク
-var calc_mark: String = ""
+var calc_num: Double = 0
+//演算のマーク用の配列
+var calc_mark: [String] = []
 //結果入れる変数
-var result_num: Int = 0
+var result_num: Double = 0
 //計算の途中かどうかのフラグ　trueなら途中
 var calc_Flg: Bool = false
-
+//演算子配列用の変数
+var i: Int = 0
 
 
 
@@ -40,111 +39,101 @@ class ViewController: UIViewController {
    
     //四則演算のボタンが押された時の処理
     @IBAction func tappedCalc(_ sender: UIButton) {
+       
+        calc_mark.append((sender.titleLabel?.text)!)
+        result.text = calc_mark[i]
         
-        calc_mark = (sender.titleLabel?.text)!
-        result.text = calc_mark
         //こめんと
         print(calc_mark)
+        if input == ""{
+            if i != 0{
+                //足し算のメソッド呼び出し
+                if calc_mark[i-1] == "+"{
+                    result_num = add(input: input, calc_num: calc_num)
+            
+                }else if calc_mark[i-1] == "-"{
+                    result_num = subtraction(input: input, calc_num: calc_num)
+         
+                }else if calc_mark[i-1] == "×"{
+                    result_num = multiplication(input: input, calc_num: calc_num)
+            
+                }else if calc_mark[i-1] == "÷"{
+                    result_num = division(input: input, calc_num: calc_num)
         
-        //足し算のメソッド呼び出し
-        if calc_mark == "+"{
-            if calc_Flg{
-                result_num = add(input: input, calc_num: calc_num)
+                }else if calc_mark[i-1] == "%"{
+                    result_num = division(input: input, calc_num: calc_num)
+            
+                }else{
+            
+                }
             }else{
                 calc_Flg = true
-                result_num = Int(input)!
-                print("＋途中結果は" + "\(result_num)")
+                result_num = Double(input)!
+                print("途中結果は" + "\(result_num)")
             }
-            
-        }else if calc_mark == "-"{
-            if calc_Flg{
-                result_num = subtraction(input: input, calc_num: calc_num)
-            }else{
-                calc_Flg = true
-                result_num = Int(input)!
-                print("-途中結果は" + "\(result_num)")
-            }
-        }else if calc_mark == "×"{
-            if calc_Flg{
-                result_num = multiplication(input: input, calc_num: calc_num)
-            }else{
-                calc_Flg = true
-                result_num = Int(input)!
-                print("×途中結果は" + "\(result_num)")
-            }
-            
-        }else if calc_mark == "÷"{
-            if calc_Flg{
-                result_num = division(input: input, calc_num: calc_num)
-            }else{
-                calc_Flg = true
-                result_num = Int(input)!
-                print("÷途中結果は" + "\(result_num)")
-            }
-            
-            
-        }else if calc_mark == "%"{
-            if calc_Flg{
-                result_num = division(input: input, calc_num: calc_num)
-            }else{
-                calc_Flg = true
-                result_num = Int(input)!
-                print("%途中結果は" + "\(result_num)")
-            }
-            
+            calc_num = result_num
+            input = ""
+            i += 1
         }else{
             
         }
-        calc_num = result_num
-        input = ""
-        
+    
     }
     
     
     //足し算の処理
-    func add(input: String, calc_num: Int) -> Int{
-        result_num = calc_num + Int(input)!
+    func add(input: String, calc_num: Double) -> Double{
+        result_num = calc_num + Double(input)!
         print("＋途中結果は" + "\(result_num)")
         return result_num
     }
     
     
     //引き算の処理
-    func subtraction(input: String, calc_num: Int) -> Int{
-        result_num = calc_num - Int(input)!
+    func subtraction(input: String, calc_num: Double) -> Double{
+        result_num = calc_num - Double(input)!
         print("マイナス途中結果は" + "\(result_num)")
         return result_num
     }
     
     
     //掛け算の処理
-    func multiplication(input: String, calc_num: Int) -> Int{
-        result_num = calc_num * Int(input)!
+    func multiplication(input: String, calc_num: Double) -> Double{
+        result_num = calc_num * Double(input)!
         print("×途中結果は" + "\(result_num)")
         return result_num
     }
     
     
     //割り算の処理
+    func division(input: String, calc_num: Double) -> Double{
+        result_num = calc_num / Double(input)!
+        print("÷途中結果は" + "\(result_num)")
+        return result_num
+    }
+ 
+    /*
+    //割り算の処理
     func division(input: String, calc_num: Int) -> Int{
         result_num = calc_num / Int(input)!
         print("÷途中結果は" + "\(result_num)")
         return result_num
     }
+*/
     
     
     @IBAction func tappedEql(_ sender: UIButton) {
         //イコールボタン押された時の処理
-        if calc_mark == "+"{
+        if calc_mark[i-1] == "+"{
             result_num = add(input: input, calc_num: calc_num)
             result.text = "\(result_num)"
-        }else if calc_mark == "-"{
+        }else if calc_mark[i-1] == "-"{
             result_num = subtraction(input: input, calc_num: calc_num)
             result.text = "\(result_num)"
-        }else if calc_mark == "×"{
+        }else if calc_mark[i-1] == "×"{
             result_num = multiplication(input: input, calc_num: calc_num)
             result.text = "\(result_num)"
-        }else if calc_mark == "÷"{
+        }else if calc_mark[i-1] == "÷"{
             result_num = division(input: input, calc_num: calc_num)
             result.text = "\(result_num)"
         }else{
@@ -158,18 +147,20 @@ class ViewController: UIViewController {
 
     @IBAction func tappedAc(_ sender: Any) {
         input = ""
-        calc_num = 0
-        calc_mark = ""
-        result_num = 0
+        calc_num = 0.0
+        calc_mark = []
+        result_num = 0.0
         calc_Flg = false
         print("全部けした！")
         result.text = "0"
+        i = 0
     }
     
     
     @IBAction func tappedBtn(_ sender: UIButton) {
+        //input += sender.title(for: UIControlState.normal)!
         //数字のボタンが押された時の処理
-        //残課題：もっと簡潔に
+        //残課題：もっと簡潔に。一旦はこれで。
         //       .は一回のみのうけつけとする！
         switch sender.tag{
         case 0:
@@ -213,14 +204,18 @@ class ViewController: UIViewController {
             result.text = input
             print(input)
         case 10:
+            if input.contains("."){
+            }else{
             input += "."
             result.text = input
             print(input)
-            
+            }
+                
         default:
             print("エラー")
             
         }
+ 
     }
     
 
